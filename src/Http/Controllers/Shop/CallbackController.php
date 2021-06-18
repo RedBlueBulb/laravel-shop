e23d2b0b612b4906840a5b67734ba8312c85913f
dejine <85028761+RedBlueBulb@users.noreply.github.com>
2021-06-18 14:59:42 +0100

    Tightening up the route controller for L6

--- src/Http/Controllers/Shop/CallbackController.php
+++ src/Http/Controllers/Shop/CallbackController.php
@@ -13,28 +13,30 @@ class CallbackController extends Controller
      * 
      * @return redirect
      */
-    protected function process(Request $request)
+    protected function process(Request $request, $status, $order_id, $shoptoken)
     {
         $validator = Validator::make(
             [
-                'order_id'  => $request->get('order_id'),
-                'status'    => $request->get('status'),
-                'shoptoken' => $request->get('shoptoken'),
+                'order_id'  => $order_id,
+                'status'    => $status,
+                'shoptoken' => $shoptoken
             ],
             [
                 'order_id'  => 'required|exists:' . config('shop.order_table') . ',id',
                 'status'    => 'required|in:success,fail',
-                'shoptoken' => 'required|exists:' . config('shop.transaction_table') . ',token,order_id,' . $request->get('order_id'),
+                'shoptoken' => 'required|exists:' . config('shop.transaction_table') . ',token,order_id,' . $order_id,
             ]
         );
+
         if ($validator->fails()) {
             abort(404);
         }
-        $order = call_user_func(config('shop.order') . '::find', $request->get('order_id'));
-        $transaction = $order->transactions()->where('token', $request->get('shoptoken'))->first();
-        Shop::callback($order, $transaction, $request->get('status'), $request->all());
+		
+        $order = call_user_func(config('shop.order') . '::find', $order_id);
+        $transaction = $order->transactions()->where('token', $shoptoken)->first();
+        Shop::callback($order, $transaction, $status, $request->all());
         $transaction->token = null;
         $transaction->save();
-        return redirect()->route(config('shop.callback_redirect_route'), ['orderId' => $order->id]);
+        return redirect()->route(config('shop.callback_redirect_route'), ['orderId' => $order->id]);        
     }
 }
